fd81be3a0b88991613325800209eb1b60261edb6
forzabarca88@gmail.com <Justin Cruze>
2026-05-09 20:52:16 +1000

    minor update

--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
 from src.utils import load_system_prompt
 
 
-MODEL = 'unsloth/qwen3.5-4b'
-BASE_URL = 'http://192.168.0.40:1234/v1'
+#MODEL = 'unsloth/qwen3.5-4b'
+MODEL = 'gemma-4-e4b-it'
+#BASE_URL = 'http://192.168.0.40:1234/v1'
+BASE_URL = 'http://127.0.0.1:1234/v1'
 API_KEY = 'fake'
 LOGGING_LEVEL = 'INFO'
 SYSTEM_PROMPT = load_system_prompt()
