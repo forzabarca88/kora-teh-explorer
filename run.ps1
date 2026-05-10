3d0c8215d1d7db559e46447900011430b4c14af1
forzabarca88@gmail.com <Justin Cruze>
2026-05-11 00:04:12 +1000

    add git pull, use git repo config

--- run.ps1
+++ run.ps1
@@ -1,9 +1,10 @@
+git pull; git checkout -f main
+
 uv run ./main.py
 
 git add *.md; git commit -m "updated by Kora"
 
 # Construct the URL with credentials
-$RemoteUrl = "https://forzabarca88:$($Env:GITHUB_TOKEN)@github.com/forzabarca88/kora-teh-explorer.git"
+#$RemoteUrl = "https://forzabarca88:$($Env:GITHUB_TOKEN)@github.com/forzabarca88/kora-teh-explorer.git"
 
-# Push using the temporary URL
-git push $RemoteUrl main
+git push
