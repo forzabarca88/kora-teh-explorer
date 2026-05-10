20c5fa31bde88d0fe1a89ea1f2f2ab9c0556628c
forzabarca88@gmail.com <Justin Cruze>
2026-05-10 12:01:42 +1000

    updates

--- src/config.py
+++ src/config.py
@@ -1,9 +1,11 @@
 from src.utils import load_system_prompt
 
+MODEL_MAPPING = {
+    'qwen3.5-0.8b': 'qwen3.5-0.8b-claude-4.6-opus-reasoning-distilled',
+    'gemma-4-e2b': 'gemma-4-e2b-it'
+}
 
-#MODEL = 'unsloth/qwen3.5-4b'
-MODEL = 'qwen3.5-0.8b-claude-4.6-opus-reasoning-distilled'
-#BASE_URL = 'http://192.168.0.40:1234/v1'
+MODEL = MODEL_MAPPING['qwen3.5-0.8b']
 BASE_URL = 'http://192.168.0.5:1234/v1'
 API_KEY = 'fake'
 LOGGING_LEVEL = 'WARNING'
