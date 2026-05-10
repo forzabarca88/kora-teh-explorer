dfa904775604a084edb00f0a925ad2f96b47d443
forzabarca88@gmail.com <Justin Cruze>
2026-05-10 12:06:54 +1000

    add env vars

--- src/config.py
+++ src/config.py
@@ -1,12 +1,14 @@
 from src.utils import load_system_prompt
+from os import getenv
 
 MODEL_MAPPING = {
     'qwen3.5-0.8b': 'qwen3.5-0.8b-claude-4.6-opus-reasoning-distilled',
     'gemma-4-e2b': 'gemma-4-e2b-it'
 }
 
-MODEL = MODEL_MAPPING['qwen3.5-0.8b']
-BASE_URL = 'http://192.168.0.5:1234/v1'
+MODEL = getenv('MODEL', default=MODEL_MAPPING['qwen3.5-0.8b'])
+BASE_URL = getenv('BASE_URL', default='http://192.168.0.5:1234/v1')
+LOGGING_LEVEL = getenv('LOGGING_LEVEL', default='INFO')
+
 API_KEY = 'fake'
-LOGGING_LEVEL = 'WARNING'
 SYSTEM_PROMPT = load_system_prompt()
